7ea897959b1c5dd781e06eeda05ead4acb1946c2
Tejas Gautam <tejasgautam004@gmail.com>
2026-06-06 17:28:24 +0530

    Add Number Guessing Game project

---
+++ GuessTheNumber/GuessTheNumber.py
@@ -0,0 +1,19 @@
+import random
+
+
+
+secretnumber = random.randint(1, 100)
+
+attempts = 0
+guess = 0
+while secretnumber != guess:
+    guess = int(input("enter a number between 1-100: "))
+    attempts += 1
+    if secretnumber > guess:
+        print("the number is higher")
+    elif secretnumber < guess:
+        print("the number is lesser")
+    else:
+        print(f"yay you guess right in {attempts} attempts")
+
+    
---
+++ README.md
@@ -0,0 +1,16 @@
+# Number Guessing Game
+
+A simple Python game where the player guesses a random number.
+
+## Features
+
+- Random number generation
+- Higher/Lower hints
+- Attempt counter
+
+## Concepts Used
+
+- Variables
+- Loops
+- Conditionals
+- Random module
